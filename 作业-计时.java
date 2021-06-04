92a444e718f432619af70f6216f67ed4380fafbb
LucyLiao211 <80941922+LucyLiao211@users.noreply.github.com>
2021-06-04 17:20:26 +0800

    Update 作业-计时.java

--- 作业-计时.java
+++ 作业-计时.java
@@ -1,11 +1,11 @@
-allChild[index].onclick = function(){//当点击目标且满足下面的条件时 让计时器时间暂停
+allChild[index].onclick = function(){
 if(gamInt == true){
 $("#tu").css("display","block");
-//计时器暂停（清除定时器）
+
 clearInterval(countdownTimer);
-//延迟2s后游戏继续 计时器暂停结束
+
 setTimeout(function(){
-// 移除所有的图片
+
 removeImgFun();
 $("#tu").css("display","none");
 imgNum++;
@@ -13,22 +13,22 @@ if(imgNum >= 10){
 imgNum = 10;
 }
 createImgFun();
-//定时器继续接之前的时间增加 继续播放（将之前的定时器重新引用一次 包括定时器的名字（必须要有 不然容易引起电脑死机）完整引入）
+
 countdownTimer = setInterval(function (){
 countdownNum -= 0.01;
-//保留两位小数
+
 countdownNum = countdownNum.toFixed(2);
 if (countdownNum <= 0){
 clearInterval(countdownTimer);
-//游戏结束
+
 alert("游戏结束！");
-//房祖名不能再点击（分数不再计数）
+
 gamInt = false;
 }
 time.innerHTML = "倒计时:" + countdownNum + "秒";
 },10);
 },2000)
-//计分
+
 scores += 1;
 score.innerHTML = "第" + scores + "关";
 if(scores > 8){
